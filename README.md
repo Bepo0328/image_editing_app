651467dfa15e9e95ebaaf6cb5c7d1633dfc9953c
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2022-03-29 22:34:07 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# image_editing_app
+ 이미지 편집 앱
---
+++ android/app/src/main/kotlin/kr/co/bepo/image_editing_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package kr.co.bepo.image_editing_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
